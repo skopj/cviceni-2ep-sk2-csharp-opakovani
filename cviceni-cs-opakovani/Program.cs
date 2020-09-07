49537335133f89d931e0daf41d1fb274d476cb24
Jakub Šenkýř <senkyr@skola.local>
2020-09-07 11:31:38 +0200

    Add project files.

---
+++ cviceni-cs-opakovani/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cviceni_cs_opakovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
